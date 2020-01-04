505ba4eb395fa5c1327eca3b7194bcf631347643
alsaba1 <salimalsaba@gmail.com>
2020-01-03 22:16:05 -0500

    First commit-project initiation

---
+++ iOS-weather-app/iOS-weather-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-weather-app
+//
+//  Created by Alsaba Salim on 1/3/20.
+//  Copyright © 2020 Alsaba Salim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
